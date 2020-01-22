ed9b48b13851075abc4a8274841c2faa93a850ee
VladPrytula <vladyslav.prytula@gmail.com>
2020-01-22 11:47:29 +0100

    some more testst

--- dl_reversi/tests/test_reversiboard_basic.py
+++ dl_reversi/tests/test_reversiboard_basic.py
@@ -18,16 +18,37 @@ class TestBasicBoard(unittest.TestCase):
     def test_init_position(self):
         self.setUp()
         assert self.reversi_board.grid_array[4, 4] == Player.white.value
+        assert self.reversi_board.grid_array[4, 5] == Player.black.value
+        assert self.reversi_board.grid_array[2, 2] == 0
         assert self.reversi_board.grid_array[0, 8] == 8
-
 
     def test_place_stone_path1(self):
         self.setUp()
         pprint(self.reversi_board.grid_array)
         assert self.reversi_board.place_stone(
-           Point(4, 6), Player.white) == True
-
+            Point(4, 6), Player.white) == True
         pprint(self.reversi_board.grid_array)
+
+        assert self.reversi_board.place_stone(
+            Point(4, 7), Player.black) == False
+        pprint(self.reversi_board.grid_array)
+        assert self.reversi_board.place_stone(
+            Point(3, 6), Player.black) == True
+        pprint(self.reversi_board.grid_array)
+
+        assert self.reversi_board.place_stone(
+            Point(4, 7), Player.white) == False
+        pprint(self.reversi_board.grid_array)
+
+        assert self.reversi_board.place_stone(
+            Point(3, 5), Player.white) == True
+        pprint(self.reversi_board.grid_array)
+
+        assert self.reversi_board.place_stone(
+            Point(5, 6), Player.black) == True
+        pprint(self.reversi_board.grid_array)
+
+
 """ 
     def test_place_stone_path1(self):
         self.setUp()
